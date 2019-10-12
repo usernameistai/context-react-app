5a35ca3ea1c5273bb5d0a3afb29ef2698bc1032f
David Battye <david2.battye@live.uwe.ac.uk>
2019-10-12 23:08:05 +0100

    Refactored LanguageContext as a Hook (function)

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,3 +1,19 @@
+import React, { createContext, useState } from 'react';
+
+export const LanguageContext  = createContext();
+
+export function LanguageProvider(props) {
+  const [language, setLanguage] = useState("spanish");
+  const changeLanguage = (e) => setLanguage(e.target.value);
+
+  return(
+    <LanguageContext.Provider value={{ language, changeLanguage }}>
+      {props.children}
+    </LanguageContext.Provider>
+  );
+}
+
+/* 
 import React, { Component, createContext } from 'react';
 
 export const LanguageContext  = createContext();
@@ -20,7 +36,7 @@ export class LanguageProvider extends Component {
   }
 }
 
-/* export const withLanguageContext = Component => props => ( // HOC higher order component
+export const withLanguageContext = Component => props => ( // HOC higher order component
   <LanguageContext.Consumer>
     {value => <Component languageContext={value} {...props} />}
   </LanguageContext.Consumer>
